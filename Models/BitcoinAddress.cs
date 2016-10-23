1e1898eefd1939cf940764f7d412e2174895bfc1
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2016-10-23 21:01:52 +0330

    add Model

--- Models/BitcoinAddress.cs
+++ Models/BitcoinAddress.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WatchOnlyBitcoinWallet
+namespace Models
 {
     public class BitcoinAddress
     {
+        /// <summary>
+        /// Name acts as a tag for the address
+        /// </summary>
+        public string Name { get; set; }
         public string Address { get; set; }
         public decimal Balance { get; set; }
-        public string Name { get; set; }
+
+        public bool Validate()
+        {
+            //Check Base58
+
+            return true;
+        }
     }
 }
