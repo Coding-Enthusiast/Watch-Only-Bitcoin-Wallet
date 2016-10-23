96f19b940907e811f668343522a545a2d36e616b
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2016-10-23 21:05:45 +0330

    add WalletServices

---
+++ WalletServices/BlockchainInfoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Models;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
+
+namespace WalletServices
+{
+    public class BlockchainInfoService : ApiCall
+    {
+        public static async void GetBalace(ObservableCollection<BitcoinAddress> bitAddrList)
+        {
+            foreach (var bitAddr in bitAddrList)
+            {
+                string url = "https://blockchain.info/address/" + bitAddr.Address + "?format=json&limit=0";
+                JObject jResult = await GetApiResponse(url);
+                decimal satoshi = 0.00000001m;
+                bitAddr.Balance = (int)jResult["final_balance"] * satoshi;
+            }
+        }
+    }
+}
--- WatchOnlyBitcoinWallet/WalletData.cs
+++ WatchOnlyBitcoinWallet/WalletData.cs
@@ -11,78 +11,22 @@ namespace WatchOnlyBitcoinWallet
 {
     public class WalletData
     {
-        private static string myFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\C.E. Watch Only Bitcoin Wallet";
-        private static string myFilePath = myFolderPath + @"\BitcoinBalance - Info.txt";
-        private static string mySettingPath = myFolderPath + @"\BitcoinBalance - Settings.txt";
-        public static List<BitcoinAddress> BitAddList = new List<BitcoinAddress>();
-        public static SettingsClass Settings = new SettingsClass();
-
-        public static void Load()
-        {
-            if (!File.Exists(myFilePath))
-            {
-                if (!Directory.Exists(myFolderPath))
-                {
-                    Directory.CreateDirectory(myFolderPath);
-                }
-                Save();
-            }
-            else
-            {
-                XmlSerializer ser = new XmlSerializer(typeof(List<BitcoinAddress>));
-                using (StreamReader st = new StreamReader(myFilePath))
-                {
-                    BitAddList = (List<BitcoinAddress>)ser.Deserialize(st);
-                }
-            }
-
-            if (!File.Exists(mySettingPath))
-            {
-                SaveSettings();
-            }
-            else
-            {
-                XmlSerializer ser2 = new XmlSerializer(typeof(SettingsClass));
-                using (StreamReader st = new StreamReader(mySettingPath))
-                {
-                    Settings = (SettingsClass)ser2.Deserialize(st);
-                }
-            }
-        }
-        public static void Save()
-        {
-            XmlSerializer ser = new XmlSerializer(typeof(List<BitcoinAddress>));
-            using (Stream str = File.Create(myFilePath))
-            {
-                ser.Serialize(str, BitAddList);
-            }
-        }
-        public static void SaveSettings()
-        {
-            XmlSerializer ser = new XmlSerializer(typeof(SettingsClass));
-            using (Stream str = File.Create(mySettingPath))
-            {
-                ser.Serialize(str, Settings);
-            }
-        }
-
-
-        public static async Task GetBalance(BitcoinAddress BTC)
-        {
-            using (var client = new HttpClient())
-            {
-                string url = "https://blockchain.info/address/" + BTC.Address + "?format=json&limit=0";
-                try
-                {
-                    BlockchainInfoAPI BitcoinInfo = JsonConvert.DeserializeObject<BlockchainInfoAPI>(await client.GetStringAsync(url));
-                    decimal balance = BitcoinInfo.final_balance * 0.00000001m;
-                    BTC.Balance = balance;
-                }
-                catch (Exception)
-                {
-                    BTC.Balance = 0;
-                }
-            }
-        }
+        //public static async Task GetBalance(BitcoinAddress BTC)
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        string url = "https://blockchain.info/address/" + BTC.Address + "?format=json&limit=0";
+        //        try
+        //        {
+        //            BlockchainInfoAPI BitcoinInfo = JsonConvert.DeserializeObject<BlockchainInfoAPI>(await client.GetStringAsync(url));
+        //            decimal balance = BitcoinInfo.final_balance * 0.00000001m;
+        //            BTC.Balance = balance;
+        //        }
+        //        catch (Exception)
+        //        {
+        //            BTC.Balance = 0;
+        //        }
+        //    }
+        //}
     }
 }
