4d99279635a9bc8a824a1b8fc0ded8e084e12036
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2016-11-01 15:46:57 +0330

    Add PriceServices for updating bitcoin price

---
+++ WalletServices/PriceServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+
+namespace WalletServices
+{
+    public class PriceServices : ApiCall
+    {
+        public enum ServiceNames
+        {
+            Bitfinex,
+            Btce
+        }
+
+        public static async Task<decimal> GetPrice(string serviceName)
+        {
+            JObject jResult;
+            decimal price;
+            var name = Enum.Parse(typeof(ServiceNames), serviceName);
+            switch ((ServiceNames)name)
+            {
+                case ServiceNames.Bitfinex:
+                    jResult = await GetApiResponse("https://api.bitfinex.com/v1/pubticker/btcusd");
+                    price = (decimal)jResult["last_price"];
+                    break;
+                case ServiceNames.Btce:
+                    jResult = await GetApiResponse("https://btc-e.com/api/3/ticker/btc_usd");
+                    price = (decimal)jResult["btc_usd"]["last"];
+                    break;
+                default:
+                    price = 0;
+                    break;
+            }
+            return price;
+        }
+    }
+}
