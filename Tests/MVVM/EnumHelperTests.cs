cfb0dc465d6dfeeb1b7dcae64e75c3cd62fb7d2d
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2025-02-11 07:17:55 +0330

    Add EnumHelper class + tests

---
+++ Tests/MVVM/EnumHelperTests.cs
@@ -0,0 +1,44 @@
+﻿// WatchOnlyBitcoinWallet Tests
+// Copyright (c) 2016 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using WatchOnlyBitcoinWallet.MVVM;
+
+namespace Tests.MVVM
+{
+    public class EnumHelperTests
+    {
+        public enum Foo
+        {
+            Foo1,
+            Foo2,
+            Foo3,
+            Foo4,
+        }
+
+        [Fact]
+        public void GetAllEnumValuesTest()
+        {
+            IEnumerable<Foo> actual = EnumHelper.GetAllEnumValues<Foo>();
+            IEnumerable<Foo> expected = new Foo[] { Foo.Foo1, Foo.Foo2, Foo.Foo3, Foo.Foo4 };
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetEnumValues_All_Test()
+        {
+            IEnumerable<Foo> actual = EnumHelper.GetEnumValues<Foo>();
+            IEnumerable<Foo> expected = new Foo[] { Foo.Foo1, Foo.Foo2, Foo.Foo3, Foo.Foo4 };
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetEnumValues_Exclude_Test()
+        {
+            IEnumerable<Foo> actual = EnumHelper.GetEnumValues(Foo.Foo2, Foo.Foo2, Foo.Foo4);
+            IEnumerable<Foo> expected = new Foo[] { Foo.Foo1, Foo.Foo3 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ WatchOnlyBitcoinWallet/MVVM/EnumHelper.cs
@@ -0,0 +1,30 @@
+﻿// WatchOnlyBitcoinWallet
+// Copyright (c) 2016 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WatchOnlyBitcoinWallet.MVVM
+{
+    public static class EnumHelper
+    {
+        public static IEnumerable<T> GetAllEnumValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static IEnumerable<T> GetEnumValues<T>(params T[] exclude) where T : Enum
+        {
+            foreach (T item in Enum.GetValues(typeof(T)))
+            {
+                if (exclude != null && !exclude.Contains(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
