5e9ea1686784a4b2fc21dfdc5103bcc2d0ceffd7
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2025-02-26 12:22:23 +0330

    Add a messagebox

---
+++ WatchOnlyBitcoinWallet/AllEnums.cs
@@ -0,0 +1,23 @@
+﻿// WatchOnlyBitcoinWallet
+// Copyright (c) 2016 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+namespace WatchOnlyBitcoinWallet
+{
+    public enum MessageBoxType
+    {
+        Ok,
+        OkCancel,
+        YesNo,
+    }
+
+
+    public enum MessageBoxResult
+    {
+        Ok,
+        Cancel,
+        Yes,
+        No
+    }
+}
