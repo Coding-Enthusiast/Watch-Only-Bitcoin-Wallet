e9627a8ae7a7d39c756b97f5b4ee2c5c0aad7cb8
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2025-03-02 09:36:27 +0330

    Remove ErrorCollection and mark DataManager obsolete

--- WatchOnlyBitcoinWallet/Services/DataManager.cs
+++ WatchOnlyBitcoinWallet/Services/DataManager.cs
@@ -3,13 +3,13 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
-using Microsoft.Win32;
 using Newtonsoft.Json;
 using System;
 using System.IO;
 
 namespace WatchOnlyBitcoinWallet.Services
 {
+    [Obsolete]
     public static class DataManager
     {
         static DataManager()
@@ -49,11 +49,9 @@ namespace WatchOnlyBitcoinWallet.Services
             T result;
             if (File.Exists(myFilePath))
             {
-                using (StreamReader st = File.OpenText(myFilePath))
-                {
-                    JsonSerializer ser = new JsonSerializer();
-                    result = (T)ser.Deserialize(st, typeof(T));
-                }
+                using StreamReader st = File.OpenText(myFilePath);
+                JsonSerializer ser = new();
+                result = (T)ser.Deserialize(st, typeof(T));
 
                 return result;
             }
@@ -62,65 +60,5 @@ namespace WatchOnlyBitcoinWallet.Services
                 return (T)Activator.CreateInstance(typeof(T));
             }
         }
-
-
-        /// <summary>
-        /// Writes data to disk.
-        /// </summary>
-        /// <typeparam name="T">Type of the data to save.</typeparam>
-        /// <param name="dataToSave">Data to save.</param>
-        /// <param name="f">Name of the folder to place the file.</param>
-        /// <param name="fileName">Name of the file to save to.</param>
-        public static void WriteFile<T>(T dataToSave, FileType f)
-        {
-            string myFilePath = string.Empty;
-            switch (f)
-            {
-                case FileType.Wallet:
-                    myFilePath = mainFolderPath + @"\Wallet.json";
-                    break;
-                case FileType.Settings:
-                    myFilePath = mainFolderPath + @"\Settings.json";
-                    break;
-            }
-
-            if (!Directory.Exists(mainFolderPath))
-            {
-                Directory.CreateDirectory(mainFolderPath);
-            }
-
-            using (StreamWriter str = File.CreateText(myFilePath))
-            {
-                JsonSerializer ser = new JsonSerializer();
-                ser.Serialize(str, dataToSave);
-            }
-        }
-
-
-        /// <summary>
-        /// Opens up default open dialog and tries reading the file.
-        /// </summary>
-        /// <returns>returns null on failure</returns>
-        public static Response<string[]> OpenFileDialog()
-        {
-            Response<string[]> resp = new Response<string[]>();
-
-            //OpenFileDialog ofd = new OpenFileDialog();
-            //ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            //ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            //if (ofd.ShowDialog() == true)
-            //{
-            //    try
-            //    {
-            //        resp.Result = File.ReadAllLines(ofd.FileName);
-            //    }
-            //    catch (Exception)
-            //    {
-            //        resp.Errors.Add("Could not read file!");
-            //    }
-            //}
-            return resp;
-        }
-
     }
 }
